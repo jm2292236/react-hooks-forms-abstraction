9b3bd70ad24e06808fb2675cb5322aeac1e3ddbe
Jaime Montes de Oca <jaime0422@live.com>
2022-06-05 16:43:30 -0700

    Completed Assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,58 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+    const [formData, setFormData] = useState({
+        firstName: "John",
+        lastName: "Henry",
+        age: "",
+        admin: false,
+    })
+    let errMsg = "";
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+    // Generic function to update input fields base on the input's names
+    function handleChange(event) {
+        const name = event.target.name;
+        let value = event.target.value;
+        
+        // For checkboxes use "checked" instead of value 
+        if (event.target.type === "checkbox") {
+            value = event.target.checked;
+        }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+        console.log(name, formData);
+    }
 
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (formData.age < 0) {
+            errMsg = "Age can't be negative";
+            console.log({errMsg});
+        } else {
+            errMsg = "";
+            console.log({errMsg});
+        }
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="firstName" onChange={handleChange} value={formData.firstName} />
+                <input type="text" name="lastName" onChange={handleChange} value={formData.lastName} />
+                <input type="number" name="age" onChange={handleChange} value={formData.age} placeholder="Age" />
+                <input type="checkbox" name="admin" onChange={handleChange} checked={formData.admin} />
+                <button type="submit">Submit</button>
+            </form>
+
+            <p>Feedback: {errMsg}</p>
+        </div>
+    );
 }
 
 export default Form;
